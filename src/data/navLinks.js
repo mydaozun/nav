5c6e70b6bb368ac7cecc4e6c5de82c04699e12c0
mydaozun <838325042@qq.com>
2026-03-08 16:00:08 +0800

    fix: sync data from Notion

--- src/data/navLinks.js
+++ src/data/navLinks.js
@@ -1,10 +1,66 @@
-export const websiteConfig = {};
+export const websiteConfig = {
+  "favicon": "https://prod-files-secure.s3.us-west-2.amazonaws.com/bd431346-c354-4ec7-9303-d2ff19b0555d/41e73a98-e58f-4269-8b8d-f22245744fd8/52DH.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=ASIAZI2LB46664LUH7C7%2F20260308%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20260308T075945Z&X-Amz-Expires=3600&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEEgaCXVzLXdlc3QtMiJHMEUCIF5Y8c3Kb92pZTfJfFC5Xd9OLVVOxSBH1L5hF9wpmjO8AiEA3HUOtZ5cRQvEIYXJ%2FSPNjxNq2NyWmnolcb9SAlsqOJYq%2FwMIERAAGgw2Mzc0MjMxODM4MDUiDFONmu9gzK%2B7IE4ImyrcA0tMfihEfb5BpSEEJHUiUTkJVhUjkTb97DVaTIG0haMHRYjkoKr1i0NMs1NHYZtOXxSwXgEGF4Sx0sCtrgaMHE611my8mIu6fZH6c5vk9zEmBUa7qjHF%2B%2Fs%2Fd8zxULM6aaeE1MGA8YmrOIw%2BkElRsrtEUarqfIQc4GtXbcsEpDvsxTI3WSOzv9ubCQTYsyzu%2FnoeUne97xHFZv0YDKPYnylSDXfmU%2BoNQk36dPXQQQ3l1cWm%2FV1p0xlnKuqH2Mm04JkRnmHbdp6quKtm%2BvH7ifbVN5EPZdkPPWXKh8b%2B%2Fgov%2B8nFAXtRS7IL9J9oNrZ%2BgNbj39FVnU4ogDeU3Lg1MGaWf%2BfYmYG7K5YJmw%2FGg9AsRwv9D8KMT6YMECfaRsT1RAmxyTlN8zPzGoEVCyMqQy4mCWoggbHHU%2FZc0dK%2Bkl7qTkUYrEGjg5iwlgE26fibD36BYXFw9U%2BA%2FDBGATTnlCxQ%2Brci8WSB4kK8o%2FU4E38Q29hnjiHQclZ9wq7IsESuv0jHF5gIVT8HubU2rYTS1WeJ%2FWRBGVJmtNROC1%2F41iTBVKpSOgkRGH%2BWLIgNm%2Fpbzr4hC2Bi2vegu%2B%2BmOUL90Fuz%2BDUBHuMyqJnZ%2BdUqN5e2H2r2Z4%2FurM3c%2BgiwMMfNtM0GOqUBgm%2BMOqXGlqqaHfqxvjEs9Q9G6%2BJpdR5VFXzx5nor5CLHEl%2ByoDf87HuNTSPlRBoCh%2F%2BL8vTVhQMu23sO0wdexDcY62uO8eKjFV4nKrhArGb3oH7Pgr%2F%2FElobJn%2BjxAmc8ghHO9KwHmBN8dFTpZz4Rc7eQrsMHZIsNb99OIZZew33a3orPEFaMLDb15c5tPAD29Gz1OEYzXRmeaaFbjFQw0H8mn4I&X-Amz-Signature=f52c1859078edd7ed0ac982b8a9e11a0863b087bed4bcdb2799c455cebe26aa5&X-Amz-SignedHeaders=host&x-amz-checksum-mode=ENABLED&x-id=GetObject",
+  "author": "mydaozun",
+  "site_monetag": "<script src=\"https://5gvci.com/act/files/tag.min.js?z=10688447\" data-cfasync=\"false\" async></script>",
+  "site_google": "<script async src=\"https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6155137717903643\"\ncrossorigin=\"anonymous\"></script>",
+  "title": "52DH导航 - 开源、自部署的私人化书签首页，打造专属你的极简导航站",
+  "social_github": "https://github.com/mydaozun",
+  "keywords": "导航站, 个人导航, 收藏夹, 自部署书签, 开源导航, 轻量工具, 开发者工具, 自托管, 极简首页, 书签管理, 在线工具, 自定义导航,,导航网站,前端资源,社区站点,设计师,实用工具,学习资源,运营,网络安全,node.js,AI工具,AI写作,AI论文,AI画图,AI转换,豆包,DeepSeek,ChatGPT,Grok,OPENCLAW",
+  "description": "免费开源的个人导航站模板，轻量部署、自动化更新，支持个性化定制，快速访问你的常用工具、书签和在线服务。适合开发者、极客、内容创作者和自托管爱好者。",
+  "social_blog": "https://52dh.cc.cd"
+};
 
 /**
  * 网站分类列表
  * @type {Array<{id: string, name: string, icon: string}>}
  */
-export const categories = [];
+export const categories = [
+  {
+    "id": "openclaw",
+    "name": "openclaw",
+    "icon": ""
+  },
+  {
+    "id": "AI工具",
+    "name": "AI工具",
+    "icon": ""
+  },
+  {
+    "id": "设计资源",
+    "name": "设计资源",
+    "icon": ""
+  },
+  {
+    "id": "文字处理",
+    "name": "文字处理",
+    "icon": ""
+  },
+  {
+    "id": "开发工具",
+    "name": "开发工具",
+    "icon": ""
+  },
+  {
+    "id": "信息源",
+    "name": "信息源",
+    "icon": ""
+  },
+  {
+    "id": "知识管理",
+    "name": "知识管理",
+    "icon": ""
+  },
+  {
+    "id": "3D打印",
+    "name": "3D打印",
+    "icon": ""
+  },
+  {
+    "id": "桌面软件",
+    "name": "桌面软件",
+    "icon": ""
+  }
+];
 /**
  * 网站列表
  * @type {Array<{id: string, title: string, description: string, shortDesc: string, url: string, icon: string, category: string}>}
